59f47edba678a2cb7dfe99cfeee0a29df8c11631
Guan <Shuo_Guan1@baylor.edu>
2021-09-20 13:26:02 -0500

    first commit

---
+++ CellPhoneProgram.py
@@ -0,0 +1,16 @@
+import CellPhoneClass
+
+
+def main():
+    man = input('Enter the manufacturer:')
+    mod = input('Enter the model number:')
+    retail = float(input('Enter the retail price:'))
+
+    phone = CellPhoneClass.Phone(man,mod,retail)
+
+    print('Here is the data that you entered:')
+    print('manufacturer:', phone.get_manufact())
+    print('model Number:',phone.get_model())
+    print('Retail Price: $', phone.get_retail_price(), ',.2f', sep = '')
+
+main()
